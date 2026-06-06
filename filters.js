426c44e22f0e99da343e175891ed7f549191b8f7
Samvel Hovhannisyan <samvel.hovhannisyan8888@gmail.com>
2026-06-06 13:27:20 +0400

    Finalize List.am hybrid Telegram bot

--- filters.js
+++ filters.js
@@ -1,38 +1,38 @@
 export const FILTERS = [
   {
     id: 'sale_apartments',
-    label: 'Վաճառք Բնակարաններ',
-    url: 'https://www.list.am/category/60',
+    label: 'Անշարժ գույք Վաճառք Բնակարաններ',
+    url: 'https://www.list.am/category/60?n=2%2C3%2C5%2C7%2C8%2C9%2C10&sname=&s=&cmtype=0&crc=&price1=&price2=&sq_price1=&sq_price2=&_a5=&_a39=&_a40=&_a85=&_a73=&_a3_1=&_a3_2=&_a4=&_a37=&_a36=&_a11_1=&_a11_2=&_a47=&_a78=&_a38=&_a82=&_a77=',
     enabledByDefault: true,
   },
   {
     id: 'sale_houses',
-    label: 'Վաճառք Տներ',
-    url: 'https://www.list.am/category/1386',
-    enabledByDefault: true,
-  },
-  {
-    id: 'rent_apartments_long',
-    label: 'Երկարաժամկետ վարձակալություն Բնակարաններ',
-    url: 'https://www.list.am/category/56?n=2%2C3%2C7%2C8&sname=&s=&cmtype=&crc=&price1=&price2=&_a5=&_a39=&_a40=&_a85=&_a73=&_a3_1=&_a3_2=&_a4=&_a37=&_a36=&_a11_1=&_a11_2=&_a47=&_a78=&_a38=&_a74=&_a75=&_a77=&_a68=&_a69=',
-    enabledByDefault: true,
-  },
-  {
-    id: 'rent_houses_long',
-    label: 'Երկարաժամկետ վարձակալություն Տներ',
-    url: 'https://www.list.am/category/1377?n=2%2C3%2C4%2C7%2C8%2C9%2C10&sname=&s=&cmtype=&crc=&price1=&price2=&_a4=&_a136_1=&_a136_2=&_a5=&_a34=&_a35_1=&_a35_2=&_a37=&_a38=&_a68=&_a69=&_a76=&_a83=&_a78=&_a75=',
+    label: 'Անշարժ գույք Վաճառք Տներ',
+    url: 'https://www.list.am/category/1386?n=2%2C3%2C4%2C5%2C7%2C8%2C9%2C10%2C11&sname=&s=&cmtype=0&crc=&price1=&price2=&_a4=&_a136_1=&_a136_2=&_a5=&_a34=&_a35_1=&_a35_2=&_a37=&_a76=&_a38=&_a82=&_a81=&_a80=&_a78=',
     enabledByDefault: true,
   },
   {
     id: 'sale_commercial',
-    label: 'Վաճառք Կոմերցիոն անշարժ գույք',
-    url: 'https://www.list.am/category/199?n=2%2C3%2C4%2C7%2C8%2C9%2C10&sname=&s=&cmtype=&crc=&price1=&price2=&_a5=&_a3_1=&_a3_2=&_a40=&_a88=&_a73=&_a89=&_a78=',
+    label: 'Անշարժ գույք Վաճառք Կոմերցիոն անշարժ գույք',
+    url: 'https://www.list.am/category/199?n=3%2C4%2C5%2C7%2C8&sname=&s=&cmtype=0&crc=&price1=&price2=&_a5=&_a3_1=&_a3_2=&_a40=&_a88=&_a73=&_a89=&_a78=',
+    enabledByDefault: true,
+  },
+  {
+    id: 'rent_apartments_long',
+    label: 'Անշարժ գույք Երկարաժամկետ վարձակալություն Բնակարաններ',
+    url: 'https://www.list.am/category/56?n=3%2C8&sname=&s=&cmtype=0&crc=&price1=&price2=&_a5=&_a39=&_a40=&_a85=&_a73=&_a3_1=&_a3_2=&_a4=&_a37=&_a36=&_a11_1=&_a11_2=&_a47=&_a78=&_a38=&_a74=&_a75=&_a77=&_a68=&_a69=',
+    enabledByDefault: true,
+  },
+  {
+    id: 'rent_houses_long',
+    label: 'Անշարժ գույք Երկարաժամկետ վարձակալություն Տներ',
+    url: 'https://www.list.am/category/1377?n=2%2C3%2C4%2C5%2C8%2C11&sname=&s=&cmtype=0&crc=&price1=&price2=&_a4=&_a136_1=&_a136_2=&_a5=&_a34=&_a35_1=&_a35_2=&_a37=&_a38=&_a68=&_a69=&_a76=&_a83=&_a78=&_a75=',
     enabledByDefault: true,
   },
   {
     id: 'rent_commercial_long',
-    label: 'Երկարաժամկետ վարձակալություն Կոմերցիոն անշարժ գույք',
-    url: 'https://www.list.am/category/59?n=2%2C3%2C4%2C7%2C8%2C9%2C10&sname=&s=&cmtype=&crc=&price1=&price2=&_a3_1=&_a3_2=&_a78=&_a73=&_a79=&_a89=&_a88=&_a40=&_a90=',
+    label: 'Անշարժ գույք Երկարաժամկետ վարձակալություն Կոմերցիոն անշարժ գույք',
+    url: 'https://www.list.am/category/59?n=3%2C8&sname=&s=&cmtype=0&crc=&price1=&price2=&_a3_1=&_a3_2=&_a78=&_a73=&_a79=&_a89=&_a88=&_a40=&_a90=',
     enabledByDefault: true,
   },
 ];
